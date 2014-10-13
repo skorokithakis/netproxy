8ddd0a25d45be127cd2711266a29555e163687f0
Stavros Korokithakis <stavros@korokithakis.net>
2014-10-13 14:39:38 +0300

    Add exception.

--- preprocess.py
+++ preprocess.py
@@ -14,11 +14,13 @@ json_config = re.sub("//.*", "", json_config, re.MULTILINE)
 config = json.loads(json_config)
 
 # Add our IP.
-external_ip = urllib2.urlopen("http://checkip.dyndns.org/").read()
-external_ip = re.search("IP Address: (.*?)<", external_ip).group(1)
-print("Detected external IP as %s, using that. If it's wrong, please replace it in config.json before building." % external_ip)
-
-config["haproxy_bind_ip"] = external_ip
+try:
+    external_ip = urllib2.urlopen("http://checkip.dyndns.org/").read()
+    external_ip = re.search("IP Address: (.*?)<", external_ip).group(1)
+    print("Detected external IP as %s, using that. If it's wrong, please replace it in config.json before building." % external_ip)
+    config["haproxy_bind_ip"] = external_ip
+except:
+    print("Could not detect external IP. Please change the haproxy_bind_ip setting in config.json before building.")
 
 # Write it out.
 open("config.json", "w").write(json.dumps(config, sort_keys=True, indent=2))
