aecf204b5bb6729ffb14c44ad8fbeb9e0d1cb9b5
Stavros Korokithakis <stavros@korokithakis.net>
2014-10-13 16:01:40 +0300

    Add arguments to preprocess.

--- preprocess.py
+++ preprocess.py
@@ -1,45 +1,58 @@
 #!/usr/bin/env python
 
+import argparse
 import json
 import re
 import string
 import random
 import urllib2
 
-print("Downloading configuration...")
 
-# Download TSDU's config.json.
-json_config = urllib2.urlopen("https://raw.githubusercontent.com/trick77/tunlr-style-dns-unblocking/master/config.json").read()
+def main(external_ip=None):
+    print("Downloading configuration...")
 
-# Remove comments.
-json_config = re.sub("//.*", "", json_config, re.MULTILINE)
-config = json.loads(json_config)
+    # Download TSDU's config.json.
+    json_config = urllib2.urlopen("https://raw.githubusercontent.com/trick77/tunlr-style-dns-unblocking/master/config.json").read()
 
-# Add our IP.
-try:
-    external_ip = urllib2.urlopen("http://checkip.dyndns.org/").read()
-    external_ip = re.search("IP Address: (.*?)<", external_ip).group(1)
-    print("Detected external IP as %s, using that. If it's wrong, please replace it in config.json before building." % external_ip)
+    # Remove comments.
+    json_config = re.sub("//.*", "", json_config, re.MULTILINE)
+    config = json.loads(json_config)
+
+    if external_ip is None:
+        # Add our IP.
+        try:
+            print("Autodetecting external IP address...")
+            external_ip = urllib2.urlopen("http://checkip.dyndns.org/").read()
+            external_ip = re.search("IP Address: (.*?)<", external_ip).group(1)
+            print("Detected external IP as %s, using that. If it's wrong, please replace it in config.json before building." % external_ip)
+        except:
+            print("Could not detect external IP. Please change the haproxy_bind_ip setting in config.json before building.")
+            external_ip = "YOUR IP HERE"
+
     config["haproxy_bind_ip"] = external_ip
-except:
-    print("Could not detect external IP. Please change the haproxy_bind_ip setting in config.json before building.")
 
-# Change the stats password.
-config["stats"]["password"] = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+    # Change the stats password.
+    config["stats"]["password"] = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
 
-print("HAProxy stats password is %s, please make a note of it." % config["stats"]["password"])
+    print("HAProxy stats password is %s, please make a note of it." % config["stats"]["password"])
 
-# Write it out.
-open("config.json", "w").write(json.dumps(config, sort_keys=True, indent=2))
+    # Write it out.
+    open("config.json", "w").write(json.dumps(config, sort_keys=True, indent=2, separators=(',', ': ')))
 
-print("Creating hosts file...")
+    print("Creating hosts file...")
 
-# Create the hosts file lines.
-lines = ["%s %s\n" % (config["haproxy_bind_ip"], proxy["dest_addr"]) for proxy in config["proxies"]]
+    # Create the hosts file lines.
+    lines = ["%s %s\n" % (config["haproxy_bind_ip"], proxy["dest_addr"]) for proxy in config["proxies"]]
 
-hosts = open("hosts", "w")
-hosts.write("\n\n# netproxy IP section\n")
-hosts.writelines(lines)
-hosts.close()
+    hosts = open("hosts", "w")
+    hosts.write("\n\n# netproxy IP section\n")
+    hosts.writelines(lines)
+    hosts.close()
 
-print("Done!")
+    print("Done!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Preprocess the netproxy config.")
+    parser.add_argument("-e", "--externalip", metavar="IP", type=str, help="The external IP of the server (autodetected if not specified)")
+    args = parser.parse_args()
+    main(external_ip=args.externalip)
